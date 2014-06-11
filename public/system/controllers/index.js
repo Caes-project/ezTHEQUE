2ef2e6a1e03bdb332d7bf972f2bab1dd6b357dea
Thomas Brillard <thomb45@gmail.com>
2014-06-11 11:14:10 +0200

    Début de la gestion des emprunts en selectionnant d'abord l'utilisateur

--- public/system/controllers/index.js
+++ public/system/controllers/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
-    $scope.global = Global;
+angular.module('mean.system').controller('IndexController', ['$scope', '$stateParams','$location','Users','Global', 'Livres', 
+    function($scope,$stateParams, $location, Users, Global, Livres) {
+   	 $scope.global = Global;
+   	 if($scope.global.isAdmin){
+       Users.query(function(users){
+            console.log(users);
+            $scope.users = users;
+        });
+    }
 }]);
--- public/system/routes/system.js
+++ public/system/routes/system.js
@@ -6,6 +6,27 @@ angular.module('mean.system').config(['$stateProvider', '$urlRouterProvider',
             // For unmatched routes:
             $urlRouterProvider.otherwise('/');
 
+
+            var checkAdmin = function($q, $timeout, $http, $location) {
+              // Initialize a new promise
+                var deferred = $q.defer();
+
+                // Make an AJAX call to check if the user is logged in
+                $http.get('/loggedin').success(function(user) {
+                    // Authenticated
+                    // console.log(user.roles);
+                    if (user.roles.indexOf('admin') !== -1) $timeout(deferred.resolve);
+
+                    // Not Authenticated
+                    else {
+                        $timeout(deferred.reject);
+                        $location.url('/login');
+                    }
+                });
+
+                return deferred.promise;
+            };
+
             // states for my app
             $stateProvider              
                 .state('home', {
@@ -14,6 +35,13 @@ angular.module('mean.system').config(['$stateProvider', '$urlRouterProvider',
                 })
                 .state('auth', {
                     templateUrl: 'public/auth/views/index.html'
+                })
+                .state('admin user', {
+                    url: '/admin/users/:userId',
+                    templateUrl: 'public/system/views/admin_user.html',
+                    resolve: {
+                        loggedin: checkAdmin
+                    }
                 });
         }
     ])
