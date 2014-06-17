927e6d8d578141d66d96ab61769108497dd551cb
Thomas Brillard <thomb45@gmail.com>
2014-06-17 13:44:29 +0200

    Dossier jeu de test ppur script insertion

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -3,7 +3,7 @@
 module.exports = {
     db: 'mongodb://localhost/mean-prod',
     app: {
-        name: 'MEAN - A Modern Stack - Production'
+        name: 'ezThèque caes 54'
     },
     facebook: {
         clientID: 'APP_ID',
