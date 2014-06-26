15a1a501dcba7272475bbc244f5d6d0f992de6cf
Thomas Brillard <thomb45@gmail.com>
2014-06-26 16:54:56 +0200

    Maj data_set update dev instead of prod

--- data_set.sh
+++ data_set.sh
@@ -1,2 +1,2 @@
-mongoimport --db mean-prod --collection livres ./jeu-test/livres.json 
-mongoimport --db mean-prod --collection users ./jeu-test/users.json 
+mongoimport --db mean-dev --collection livres ./jeu-test/livres.json 
+mongoimport --db mean-dev --collection users ./jeu-test/users.json 
